332b73c7f6efb7696ce177b0338d6d2e631bebe7
dullacodes5853 <abdullahgilani5853@gmail.com@gmail.com>
2023-07-05 19:05:22 -0400

    pic

--- src/components/TermsAndConditions/index.js
+++ src/components/TermsAndConditions/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
 import { styled } from '@mui/system';
-import backgroundImage from '/Users/dullasiphone/delaydollar/src/assets/term.jpg';
+import backgroundImage from '/Users/dullasiphone/delaydollar/src/assets/yes.jpg';
 
 const Background = styled('div')({
   position: 'fixed',
